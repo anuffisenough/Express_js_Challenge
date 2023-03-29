ec6aa7341aa1f742042d53e85f2f5b11b8e05892
anuffisenough <uffmanch@gmail.com>
2023-03-29 16:56:36 -0600

    fixed PORT variable

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const express = require('express');
 const path = require('path');
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 const db = require('./db/db.json');
 const uuid = require('./helpers/uuid');
 
